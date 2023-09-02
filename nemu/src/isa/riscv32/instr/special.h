d146306ca66535b2dcad042fc7685ca4fd875d4b
w <w>
2023-09-02 10:28:41 +0800

    navy-apps ics2021 initialized
    navy-apps e2d345090b759898403aaeefe96bd46c580efeaf Merge pull request #5 from unlsycn/master

--- nemu/src/isa/riscv32/instr/special.h
+++ nemu/src/isa/riscv32/instr/special.h
@@ -5,3 +5,22 @@ def_EHelper(inv) {
 def_EHelper(nemu_trap) {
   rtl_hostcall(s, HOSTCALL_EXIT, NULL, &gpr(10), NULL, 0); // gpr(10) is $a0
 }
+
+def_EHelper(csrrw) {
+  // rtl_addi(s, ddest, dsrc1, 0);
+  rtl_mv(s, dsrc1, dsrc2);
+}
+
+def_EHelper(csrrs) {
+  rtl_mv(s, ddest, dsrc1);
+  // rtl_addi(s, dsrc1, dsrc2, 0);
+}
+
+def_EHelper(ecall) {
+  rtl_j(s, isa_raise_intr(gpr(17), s->pc));
+}
+
+def_EHelper(mret) {
+  rtl_addi(s, s0, &(cpu.mepc), 4);
+  rtl_jr(s, s0);
+}
--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -4,8 +4,10 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-
-  return 0;
+  // panic("%d %x\n", NO, NO);
+  cpu.mcause = NO;
+  cpu.mepc = epc;
+  return cpu.mtvec;
 }
 
 word_t isa_query_intr() {
